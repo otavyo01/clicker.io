ab3dc297c3cbf59446334812b2ac9d2a11c144f4
otavyo01 <otavyo.pereira@escola.pr.gov.br>
2024-09-18 17:48:16 -0300

    criando testes com transicao e falhando miseravelmente

--- testes/jsteste.js
+++ testes/jsteste.js
@@ -8,4 +8,27 @@ function ele(){
     elemento.className = "descricao";
 };
 
+
+function discr(){
+    const ebut = document.createElement("h1")
+    const eh2 = document.createElement("h2")
+
+    document.body.appendChild(ebut)
+    
+    ebut.innerText = "coloca o mouse aqui"
+    ebut.className = "msover"
+
+    eh2.innerText = "algo"
+    eh2.className = "disc"
+
+    ebut.addEventListener("mouseover", () => {
+        document.body.appendChild(eh2)
+    })
+
+    ebut.addEventListener("mouseout", () => {
+        document.body.removeChild(eh2)
+    })
+}
+
 ele()
+discr()
