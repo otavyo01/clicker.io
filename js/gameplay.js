29b9a608205b8fa86e0566b40dc2ff5826e8c0da
ykmath <matheusmesquitay@gmail.com>
2024-09-17 23:31:22 -0400

    adicionado visual a loja, animaçao ao receber pontos

--- js/gameplay.js
+++ js/gameplay.js
@@ -1,13 +1,14 @@
 import perks from "./perks.js";
 
 const vezesDormidasText = document.querySelector("#gameplay-num");
+const gameplay = document.querySelector("#gameplay");
 const gameplayCama = document.querySelector("#gameplay-cama");
 
 let vezesDormidas = 0;
 
-let verificacompratempo = false;
-let verificacompradormida = false;
 let cooldown = false;
+
+let pontosPorClick = 1;
 
 var tempo = 500;
 
@@ -16,17 +17,36 @@ gameplayCama.addEventListener("click", dormir);
 function dormir() {
     if (cooldown) return; 
 
-    vezesDormidas++;
+    vezesDormidas += pontosPorClick;
 
     vezesDormidasText.textContent = vezesDormidas;
     updateCompras();
-
-    if (verificacompradormida) vezesDormidas++;
+    receberPontosAnimacao();
 
     cooldown = true;
     setTimeout(() => {
         cooldown = false; 
     }, tempo);
+}
+
+function receberPontosAnimacao() {
+    const obj = document.createElement("h2");
+    obj.style.position = "absolute";
+
+    obj.style.left = Math.round(Math.random() * gameplay.getBoundingClientRect().width) + "px";
+
+    obj.style.top = Math.round(Math.random() * gameplay.getBoundingClientRect().height) + "px";
+
+    obj.className = "pontoAnimacao";
+    obj.innerText = `+${pontosPorClick}`;
+
+    obj.style.pointerEvents = "none";
+
+    gameplay.appendChild(obj);
+
+    setTimeout(() => {
+        obj.remove();
+    }, 2000);
 }
 
 function updateCompras() {
