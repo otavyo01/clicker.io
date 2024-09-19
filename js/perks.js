f02f6be7a6f872a76f1e1fd1e61c98d7928e219d
otavyo01 <otavyo.pereira@escola.pr.gov.br>
2024-09-19 13:29:30 -0300

    adicionado nova perk na loja de perks, 'autoclicker v1'

--- js/perks.js
+++ js/perks.js
@@ -5,10 +5,18 @@ const perks = [
         custo: 100,
         level: 0,
     },
+
     {
         nome: "multiplicador",
         desc: "aumenta o numero de dormidas por click",
         custo: 200,
+        level: 0,
+    },
+    
+    {
+        nome: "autoclicker v1",
+        desc: "clica automaticamente fazendo gerar pontos.",
+        custo: 10000,
         level: 0,
     }
 ]
