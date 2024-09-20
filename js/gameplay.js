1a713efa55bafc157ca3f812873a999d9d88dcb4
otavyo01 <otavyo.pereira@escola.pr.gov.br>
2024-09-19 22:29:32 -0300

    sai merge

--- js/gameplay.js
+++ js/gameplay.js
@@ -90,28 +90,6 @@ function comprar(item) {
 
 }
 
-function teste() {
-
-    const iddelay = document.querySelector("#perk-delay");
-
-    const item = perks[0].nome; 
-    const descr = perks[0].desc
-    const elt = iddelay.querySelector(".perk-desc-titulo");
-    const eld = iddelay.querySelector(".perk-desc-texto");
-    
-    if (elt) {
-        elt.innerText = item;
-    }
-
-    if (eld){
-        eld.innerText = descr;
-    }
-
-    console.log(el);
-}
-
-teste();
-
 // function maistempo() {
 //     if(valor >= 100){
 //         comprado = Math.max(0, valor - 100);
